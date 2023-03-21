0016fe9643884dfcfb6b05045d8a5f0de90c035f
Michael Irvine <michael.j.irvine@gmail.com>
2023-03-21 11:58:21 -0400

    updates MetabaseQuery type

--- src/types/metabase.ts
+++ src/types/metabase.ts
@@ -4,10 +4,54 @@ export type MetabaseMetric = {
   id: string;
 };
 
+type Field = ['field', number]; // the field ID
+type Metric = ['metric', number]; // the metric ID
+
+type AggregationType =
+  | 'avg'
+  | 'count'
+  | 'count-where'
+  | 'cum-count'
+  | 'cum-sum'
+  | 'distinct'
+  | 'stddev'
+  | 'sum'
+  | 'sum-where'
+  | 'min'
+  | 'max';
+type AggregationClause = [AggregationType, Field] | Metric;
+
+type FilterType =
+  | 'not'
+  | '='
+  | '!='
+  | '<'
+  | '>'
+  | '<='
+  | '>='
+  | 'is-null'
+  | 'not-null'
+  | 'between'
+  | 'inside'
+  | 'starts-with'
+  | 'contains'
+  | 'does-not-contain'
+  | 'ends-with'
+  | 'time-interval';
+type FilterClause =
+  | [FilterType, Field, number | boolean | string | null]
+  | ['and' | 'or', FilterClause];
+
 export type MetabaseQuery = {
-  database: number;
+  type: 'query';
   query: {
     'source-table': number;
+    fields: Field[];
+    aggregation: AggregationClause[];
+    breakout: Field[];
+    filter: FilterClause[];
+    limit?: number;
   };
-  type: 'query';
+  database: number;
+  parameters: [];
 };
