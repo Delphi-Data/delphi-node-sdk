5e7cfee8649bfde78eaa8c6fa4c2f3d8dc113cf8
Michael Irvine <michael.j.irvine@gmail.com>
2023-12-11 09:27:29 -0500

    adds pivot and realtime ga4 reports

--- src/types/ga4.ts
+++ src/types/ga4.ts
@@ -1,126 +1,204 @@
-export type GA4Query = {
-  dimensions: string[];
-  metrics: string[];
-  dateRanges: {
-    startDate: string;
-    endDate: string;
-  }[];
-  dimensionFilter?: {
-    filters: {
-      dimensionName: string;
-      operator:
-        | 'EQUALS'
-        | 'NOT_EQUALS'
-        | 'IN_LIST'
-        | 'NOT_IN_LIST'
-        | 'REGEXP'
-        | 'BEGINS_WITH'
-        | 'ENDS_WITH'
-        | 'PARTIAL'
-        | 'EXACT'
-        | 'CONTAINS'
-        | 'IN_LIST_ICASE'
-        | 'NOT_IN_LIST_ICASE'
-        | 'EXISTS'
-        | 'NUMERIC_GREATER_THAN'
-        | 'NUMERIC_GREATER_THAN_EQUAL'
-        | 'NUMERIC_LESS_THAN'
-        | 'NUMERIC_LESS_THAN_EQUAL'
-        | 'NUMERIC_BETWEEN'
-        | 'NUMERIC_EQUAL'
-        | 'NUMERIC_IN_LIST';
-      expressions: string[];
-      caseSensitive: boolean;
-    }[];
-  };
-  metricFilter?: {
-    filters: {
-      metricName: string;
-      operator:
-        | 'EQUAL'
-        | 'LESS_THAN'
-        | 'LESS_THAN_OR_EQUAL'
-        | 'GREATER_THAN'
-        | 'GREATER_THAN_OR_EQUAL'
-        | 'BETWEEN'
-        | 'IN_LIST';
-      comparisonValue: string;
-    }[];
-  };
-  orderBys?: {
-    fieldName: string;
-    orderType:
-      | 'ORDER_TYPE_UNSPECIFIED'
-      | 'ALPHANUMERIC'
-      | 'CASE_INSENSITIVE_ALPHANUMERIC'
-      | 'NUMERIC'
-      | 'CASE_INSENSITIVE_NUMERIC';
-    sortOrder: 'ORDER_UNSPECIFIED' | 'ASCENDING' | 'DESCENDING';
-  }[];
-  currencyCode?: string;
-  cohortSpec?: {
-    cohorts: {
-      name: string;
-      dimension: string;
-      dateRange: {
-        startDate: string;
-        endDate: string;
-      };
-    }[];
-    cohortsRange: {
-      granularity: 'GRANULARITY_UNSPECIFIED' | 'DAILY' | 'WEEKLY' | 'MONTHLY';
-      startOffset: number;
-      endOffset: number;
-    };
-  };
-  keepEmptyRows?: boolean;
-  returnPropertyQuota?: boolean;
-  samplingLevel?:
-    | 'SAMPLING_UNSPECIFIED'
-    | 'DEFAULT'
-    | 'SMALL'
-    | 'LARGE'
-    | 'HIGHER_PRECISION';
-  pageToken?: string;
-  pageSize?: number;
-  includeEmptyRows?: boolean;
-  hideTotalRows?: boolean;
-  hideValueRanges?: boolean;
-  metricAggregations?:
-    | 'METRIC_AGGREGATION_UNSPECIFIED'
-    | 'TOTAL'
-    | 'MINIMUM'
-    | 'MAXIMUM'
-    | 'COUNT'
-    | 'SUM'
-    | 'STANDARD_DEVIATION'
-    | 'COUNT_DISTINCT'
-    | 'COUNT_TRUE'
-    | 'COUNT_FALSE'
-    | 'FRACTION'
-    | 'NUMERIC_MAX'
-    | 'NUMERIC_MIN'
-    | 'NUMERIC_SUM'
-    | 'NUMERIC_AVERAGE'
-    | 'EXPRESSION';
-  dimensionAggregations?:
-    | 'DIMENSION_AGGREGATION_UNSPECIFIED'
-    | 'AUTO'
-    | 'TOTAL'
-    | 'MINIMUM'
-    | 'MAXIMUM'
-    | 'COUNT'
-    | 'COUNT_DISTINCT'
-    | 'NUMERIC_MIN'
-    | 'NUMERIC_MAX'
-    | 'NUMERIC_SUM'
-    | 'NUMERIC_AVERAGE';
+export type GA4Query = Report | PivotReport | RealtimeReport;
+
+type Dimension = {
+  name: string;
+  dimensionExpression?: DimensionExpression;
+};
+
+type DimensionExpression = {
+  lowerCase?: LowerCase;
+  upperCase?: UpperCase;
+  concatenate?: Concatenate;
+  dimensionName?: DimensionName;
+  caseExpression?: CaseExpression;
+  extract?: Extract;
+};
+
+type LowerCase = {
+  sourceProperty?: string;
+};
+
+type UpperCase = {
+  sourceProperty?: string;
+};
+
+type Concatenate = {
+  dimensionNames?: string[];
+  delimiter?: string;
+};
+
+type DimensionName = {
+  dimensionName?: string;
+};
+
+type CaseExpression = {
+  dimensionName?: string;
+  cases?: Case[];
+  defaultValue?: string;
+};
+
+type Case = {
+  value?: string;
+  dimensionName?: string;
+};
+
+type Extract = {
+  sourceProperty?: string;
+  regex?: string;
+};
+
+type Metric = {
+  name: string;
+  expression?: MetricExpression;
+};
+
+type MetricExpression = {
+  concatenate?: Concatenate;
+  expression?: string;
+};
+
+type DateRange = {
+  startDate: string;
+  endDate: string;
+};
+
+type FilterExpression = {
+  andGroup?: FilterGroup;
+  orGroup?: FilterGroup;
+};
+
+type FilterGroup = {
+  filters?: Filter[];
+};
+
+type Filter = {
+  fieldName?: string;
+  stringFilter?: StringFilter;
+  inListFilter?: InListFilter;
+  numericFilter?: NumericFilter;
+  betweenFilter?: BetweenFilter;
+  nullFilter?: NullFilter;
+  notFilter?: FilterExpression;
+};
+
+type StringFilter = {
+  value?: string;
+  caseSensitive?: boolean;
+  matchType?: MatchType;
+};
+
+type InListFilter = {
+  values?: string[];
+};
+
+type NumericFilter = {
+  operation?: NumericFilterOperation;
+  value?: number;
+};
+
+type BetweenFilter = {
+  fromValue?: number;
+  toValue?: number;
+};
+
+type NullFilter = {
+  operator?: NullFilterOperator;
+};
+
+type OrderBy = {
+  dimension?: DimensionOrderBy;
+  metric?: MetricOrderBy;
+};
+
+type DimensionOrderBy = {
+  dimensionName?: string;
+  orderType?: OrderType;
+};
+
+type MetricOrderBy = {
+  metricName?: string;
+  orderType?: OrderType;
+};
+
+type Pivot = {
+  fieldNames?: string[];
+  limit?: number;
+  metricAggregations?: MetricAggregation[];
+  orderBys?: OrderBy[];
+};
+
+type MetricAggregation = {
+  name?: string;
+  expression?: string;
+};
+
+enum MatchType {
+  MATCH_TYPE_UNSPECIFIED = 'MATCH_TYPE_UNSPECIFIED',
+  EXACT = 'EXACT',
+  CONTAINS = 'CONTAINS',
+  BEGINS_WITH = 'BEGINS_WITH',
+  ENDS_WITH = 'ENDS_WITH',
+  PARTIAL = 'PARTIAL',
+  REGEXP = 'REGEXP',
+  WILDCARD = 'WILDCARD',
+}
+
+enum NumericFilterOperation {
+  OPERATOR_UNSPECIFIED = 'OPERATOR_UNSPECIFIED',
+  EQUAL = 'EQUAL',
+  LESS_THAN = 'LESS_THAN',
+  LESS_THAN_OR_EQUAL = 'LESS_THAN_OR_EQUAL',
+  GREATER_THAN = 'GREATER_THAN',
+  GREATER_THAN_OR_EQUAL = 'GREATER_THAN_OR_EQUAL',
+}
+
+enum NullFilterOperator {
+  NULL_OPERATOR_UNSPECIFIED = 'NULL_OPERATOR_UNSPECIFIED',
+  IS_NULL = 'IS_NULL',
+  IS_NOT_NULL = 'IS_NOT_NULL',
+}
+
+enum OrderType {
+  ORDER_TYPE_UNSPECIFIED = 'ORDER_TYPE_UNSPECIFIED',
+  ALPHANUMERIC = 'ALPHANUMERIC',
+  CASE_INSENSITIVE_ALPHANUMERIC = 'CASE_INSENSITIVE_ALPHANUMERIC',
+  NUMERIC = 'NUMERIC',
+}
+
+type Report = {
+  type?: 'REPORT';
+  dimensions: Dimension[];
+  metrics: Metric[];
+  dateRanges: DateRange[];
+  dimensionFilter?: FilterExpression;
+  metricFilter?: FilterExpression;
   offset?: number;
-  returnResponse?: boolean;
-  returnRowTotals?: boolean;
-  returnRowsPerQuery?: number;
-  viewId?: string;
-  property?: string;
-  quotaUser?: string;
-  userIp?: string;
+  limit?: number;
+  metricAggregations?: MetricAggregation[];
+  orderBys?: OrderBy[];
+};
+
+type PivotReport = {
+  type: 'PIVOT';
+  dimensions: Dimension[];
+  metrics: Metric[];
+  dateRanges: DateRange[];
+  dimensionFilter?: FilterExpression;
+  metricFilter?: FilterExpression;
+  offset?: number;
+  limit?: number;
+  metricAggregations?: MetricAggregation[];
+  orderBys?: OrderBy[];
+  pivots: Pivot[];
+};
+
+type RealtimeReport = {
+  type: 'REALTIME';
+  dimensions: Dimension[];
+  metrics: Metric[];
+  dimensionFilter?: FilterExpression;
+  metricFilter?: FilterExpression;
+  limit?: number;
+  metricAggregations?: MetricAggregation[];
+  orderBys?: OrderBy[];
 };
